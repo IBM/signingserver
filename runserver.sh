2960c9e2ba5a2af3522a5a830bc9d0c328619151
Brian Fabec <bfabec@users.noreply.github.com>
2025-02-26 06:52:18 -0600

    Add key generation to start server script (#8)
    * Update runserver.sh
    
    * Update runserver.sh

--- runserver.sh
+++ runserver.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/bash
 export KEYSTORE_PATH=/data/signingservice/keys
 mkdir -p $KEYSTORE_PATH
-/wlp/bin/server run default
+/wlp/bin/server start default
+
+if [ -z "$(ls -A /data/signingservice/keys)" ]; then
+  echo "Creating default key pairs"
+  curl --request POST --url http://localhost:9080/signing/api/v2/keys?type=ECDSA_SECP256K1 --header 'Content-Type: application/json' --data '{}'
+  echo -e "\n"
+  curl --request POST --url http://localhost:9080/signing/api/v2/keys?type=EDDSA_ED25519 --header 'Content-Type: application/json' --data '{}'
+  echo -e "\n"
+fi
+
+tail -f -n +1 /wlp/usr/servers/default/logs/console.log
